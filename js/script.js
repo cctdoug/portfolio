e3af6c6ed68f12bed3a249f50446cd0f5bcb977c
dacsantos <72028806+dacsantos@users.noreply.github.com>
2023-05-14 22:18:55 +0100

    All main sections done

--- js/script.js
+++ js/script.js
@@ -14,3 +14,29 @@ scrollLinks.forEach((link) => {
   link.addEventListener("click", scrollToSection);
 });
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusImages(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentImage(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("images-group");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
