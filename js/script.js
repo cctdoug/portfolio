ce4c948b2368ff9f1f70abf47e5bfeb5430ab671
dacsantos <72028806+dacsantos@users.noreply.github.com>
2023-05-14 02:28:56 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function scrollToSection(event) {
+  event.preventDefault();
+
+  const target = document.querySelector(this.getAttribute("href"));
+  const offsetTop = target.offsetTop;
+  window.scrollTo({
+    top: offsetTop,
+    behavior: "smooth",
+  });
+}
+
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", scrollToSection);
+});
+
