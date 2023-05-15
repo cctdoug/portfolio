6f62e738f9d6e0b0f3509c6e791f0dee86e85e49
dacsantos <72028806+dacsantos@users.noreply.github.com>
2023-05-15 11:55:20 +0100

    project-done

--- js/script.js
+++ js/script.js
@@ -42,6 +42,41 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
 }
 
+// Change images of the slideswho overtime
+var images = [];
+var texts = [];
+
+images[0] = 'img/equations-calc.jpg';
+images[1] = 'img/equations-calc00.jpg';
+images[2] = 'img/equations-calc01.jpg';
+
+texts[0] = 'Login Page';
+texts[1] = 'Calculator';
+texts[2] = 'History';
+
+var index = 0;
+
+function change() {
+  var slideshow = document.querySelector('.images-group');
+  slideshow.classList.add('fade-out');
+
+  setTimeout(function() {
+    document.getElementById("main-photo").src = images[index];
+    document.getElementById("main-text").textContent = texts[index];
+
+    slideshow.classList.remove('fade-out');
+  }, 500); // Adjust the delay to match the CSS transition duration
+
+  if (index == 2) {
+    index = 0;
+  } else {
+    index++;
+  }
+
+  setTimeout(change, 3000);
+}
+
+window.onload = change();
 // Clear placeholder
 const input = document.getElementById("msg-box");
 
